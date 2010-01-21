51468a7abeff1be050d176c58c2fba453f18c1ca
dotted1337 <dotted1337@bb1d5238-0070-11df-b4af-25c34a55dae2>
2010-01-21 20:14:28 +0000

    Initial import

---
+++ trunk/includes/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+# Server settings
+$server_host		= "chat.eu.freenode.net";
+$server_port		= "6667";
+
+# Bot settings
+$bot_nickname		= "Dotbot";
+$bot_realname		= "Dotty's little botty";
+$bot_usermode		= 0;
+$bot_username		= "Dotbot";
+
+# Channel settings
+$channel_list		= array("#wwtest");
+?>
